6e96313a40354f254f842065398f3e808dc5c989
seth-paxton <sethpaxton@gmail.com>
2014-04-08 16:03:51 -0600

    Finished Script

--- syslog_gen.py
+++ syslog_gen.py
@@ -9,6 +9,16 @@ test open source logging solutions.
 import socket
 import argparse
 import random
+import sys
+import time
+
+"""
+Modify these variables to change the hostname, domainame, and tag
+that show up in the log messages. 
+"""   
+hostname = "host"
+domain_name = ".example.com"
+tag = ["kernel", "python", "suricata"]
 
 def syslog_sender(message, host, port):
 	try:
@@ -18,7 +28,30 @@ def syslog_sender(message, host, port):
 	finally:
 		sock.close()
 
+def open_sample_log(sample_log):
+	try:
+		with open(sample_log, 'r') as sample_log_file:
+			random_logs = random.choice(list(sample_log_file))
+			return random_logs
+	except FileNotFoundError:
+		print("[+] ERROR: Please specify valid filename")
+		return sys.exit()
 
+def random_syslogs():
+	for message in range(1, args.count+1):
+
+			time_output = time.strftime("%b %d %H:%M:%S")
+			random_host = random.choice(range(1,11))
+			random_tag = random.choice(tag)
+
+			message = open_sample_log(args.file)
+			syslog_message = "{0} {1}{2}{3} {4}: {5}".format\
+			(time_output, hostname, random_host, domain_name,\
+				random_tag, message)
+
+			syslog_sender(syslog_message, args.host, args.port)
+
+			print("[+] Sent: {0}".format(syslog_message), end='')
 
 
 if __name__ == "__main__":
@@ -32,27 +65,22 @@ if __name__ == "__main__":
 						help="Read messages from file")
 	parser.add_argument("--count", type=int, required=True,
 						help="Number of messages to send")
-	parser.add_argument("--batch", help="Use with count flag to send \
-		                X messages in X interval, batch being interval")
+	parser.add_argument("--sleep", type=float, help="Use with count flag to \
+		                send X messages every X seconds, sleep being seconds")
 	
 	args = parser.parse_args()
-		(args.host, args.port))
-	
-	try:
-		with open(args.file, 'r') as sample_log:
-			random_logs = random.sample(list(sample_log), args.count)
-	except FileNotFoundError:
-		print("Please specify valid file name")
-		exit(1)
-
-	print("[+] Sending {0} messages to {1} on port {2}".format
+		
+	if args.sleep:
+		print("[+] Sending {0} messages every {1} seconds to {2} on port {3}"\
+			.format(args.count, args.sleep, args.host, args.port))
+		try:
+			while True:
+				random_syslogs()
+				time.sleep(args.sleep)
+		except KeyboardInterrupt:
+			    # Use ctrl-c to stop the loop
+				print("[+] Stopping syslog generator...")
+	else:
+		print("[+] Sending {0} messages to {1} on port {2}".format
 				(args.count, args.host, args.port))
-	for message in random_logs:
-		syslog_sender(message, args.host, args.port)
-
-
-'''
-TODO:
-
-Need to have options for burst that requires delay
-'''
+		random_syslogs()
