922989511565e9909ee2ed0c20a1bdb6a2dfd226
seth-paxton <sethpaxton@gmail.com>
2014-04-09 17:25:33 -0600

    Fixed formatting from text editor

--- syslog_gen.py
+++ syslog_gen.py
@@ -24,80 +24,80 @@ tag = ["kernel", "python", "ids", "ips"]
 syslog_level = ["info", "error", "warn", "critical"]
 
 def raw_udp_sender(message, host, port):
-	# Stubbed in or later use
-	try:
-	    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-	    message = bytes(message, 'UTF-8')
-	    send = sock.sendto(message, (host, port))
-	finally:
-		sock.close()
+    # Stubbed in or later use
+    try:
+        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        message = bytes(message, 'UTF-8')
+        send = sock.sendto(message, (host, port))
+    finally:
+        sock.close()
 
 def open_sample_log(sample_log):
-	try:
-		with open(sample_log, 'r') as sample_log_file:
-			random_logs = random.choice(list(sample_log_file))
-			return random_logs
-	except FileNotFoundError:
-		print("[+] ERROR: Please specify valid filename")
-		return sys.exit()
+    try:
+        with open(sample_log, 'r') as sample_log_file:
+            random_logs = random.choice(list(sample_log_file))
+            return random_logs
+    except FileNotFoundError:
+        print("[+] ERROR: Please specify valid filename")
+        return sys.exit()
 
 def syslogs_sender():
-	# Initalize SysLogHandler
-	logger = logging.getLogger()
-	logger.setLevel(logging.INFO)
-	syslog = SysLogHandler(address=(args.host, args.port))
-	logger.addHandler(syslog)
+    # Initalize SysLogHandler
+    logger = logging.getLogger()
+    logger.setLevel(logging.INFO)
+    syslog = SysLogHandler(address=(args.host, args.port))
+    logger.addHandler(syslog)
 
-	for message in range(1, args.count+1):
-			# Randomize some fields
-			time_output = time.strftime("%b %d %H:%M:%S")
-			random_host = random.choice(range(1,11))
-			random_tag = random.choice(tag)
-			random_level = random.choice(syslog_level)
+    for message in range(1, args.count+1):
+        # Randomize some fields
+        time_output = time.strftime("%b %d %H:%M:%S")
+        random_host = random.choice(range(1,11))
+        random_tag = random.choice(tag)
+        random_level = random.choice(syslog_level)
 
-			message = open_sample_log(args.file)
+        message = open_sample_log(args.file)
 	
-			format = logging.Formatter\
-				('%(asctime)s {0}{1}{2} {3} %(levelname)s %(message)s'\
-				.format(hostname, random_host, domain_name, random_tag))
-			syslog.setFormatter(format)
+        format = logging.Formatter\
+                ('%(asctime)s {0}{1}{2} {3} %(levelname)s %(message)s'\
+                .format(hostname, random_host, domain_name, random_tag))
+        syslog.setFormatter(format)
 			
-			print("[+] Sent: {0}: {1}".format(time_output, message), end='')
+        print("[+] Sent: {0}: {1}".format(time_output, message), end='')
 
-			getattr(logger, random_level)(message)
+        getattr(logger, random_level)(message)
 
-	logger.removeHandler(syslog)
-	syslog.close()
+    logger.removeHandler(syslog)
+    syslog.close()
 
 
 
 if __name__ == "__main__":
 
-	parser = argparse.ArgumentParser()
-	parser.add_argument("--host", required=True,
-						help="Remote host to send messages")
-	parser.add_argument("--port", type=int, required=True,
-						help="Remote port to send messages")
-	parser.add_argument("--file", required=True, 
-						help="Read messages from file")
-	parser.add_argument("--count", type=int, required=True,
-						help="Number of messages to send")
-	parser.add_argument("--sleep", type=float, help="Use with count flag to \
-		                send X messages every X seconds, sleep being seconds")
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--host", required=True,
+                        help="Remote host to send messages")
+    parser.add_argument("--port", type=int, required=True,
+                        help="Remote port to send messages")
+    parser.add_argument("--file", required=True, 
+                        help="Read messages from file")
+    parser.add_argument("--count", type=int, required=True,
+                        help="Number of messages to send")
+    parser.add_argument("--sleep", type=float, help="Use with count flag to \
+                        send X messages every X seconds, sleep being seconds")
 	
-	args = parser.parse_args()
+    args = parser.parse_args()
 		
-	if args.sleep:
-		print("[+] Sending {0} messages every {1} seconds to {2} on port {3}"\
-			.format(args.count, args.sleep, args.host, args.port))
-		try:
-			while True:
-				syslogs_sender()
-				time.sleep(args.sleep)
-		except KeyboardInterrupt:
-			    # Use ctrl-c to stop the loop
-				print("[+] Stopping syslog generator...")
-	else:
-		print("[+] Sending {0} messages to {1} on port {2}".format
-				(args.count, args.host, args.port))
-		syslogs_sender()
+    if args.sleep:
+        print("[+] Sending {0} messages every {1} seconds to {2} on port {3}"\
+            .format(args.count, args.sleep, args.host, args.port))
+        try:
+            while True:
+                syslogs_sender()
+                time.sleep(args.sleep)
+        except KeyboardInterrupt:
+            # Use ctrl-c to stop the loop
+            print("[+] Stopping syslog generator...")
+    else:
+        print("[+] Sending {0} messages to {1} on port {2}".format
+            (args.count, args.host, args.port))
+        syslogs_sender()
